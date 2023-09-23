58e0a5fb0f9771740bc06787da90cc32168ff872
myrteza <myrteza@hotmail.fr>
2023-09-23 19:49:37 +0200

    backend try 2 done - by copying

--- src/server.js
+++ src/server.js
@@ -1,46 +1,70 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
 const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose'); 
+const app = express(); 
+app.use(cors()); 
+app.use(bodyParser.json()); 
+const port = 5000; 
+const dbUri = 'mongodb://localhost/todolist'
+const dbConfig = {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+}; 
 
-app.use(cors());
-app.use(bodyParser.json());
+mongoose.connect(dbUri, dbConfig); 
 
-mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection; 
+
+db.on('error', (err) => {
+  console.error("Error connecting to MongoDB", err); 
+})
+
+db.once('open', () => {
+  console.log("Connected to MongoDB"); 
+}); 
+
+db.on("disconnected", () => {
+  console.log('Disconnected from MongoDB'); 
+})
 
 const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
+  title: String, 
+  description: String, 
   completed: Boolean,
-});
+}); 
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('Task', taskSchema); 
 
 app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
+  const tasks = await Task.find(); 
+  res.json(tasks)
+})
 
 app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body);
-  await task.save();
-  res.json(task);
-});
+  const task = new Task(req.body); 
+  await task.save(); 
+  res.json({
+    message: "Task updated successfully",
+  }); 
+}); 
 
-app.put('/tasks/:taskId', async (req, res) => {
-  const { taskId } = req.params;
-  await Task.findByIdAndUpdate(taskId, req.body);
-  res.json({ message: 'Task updated successfully' });
-});
+app.put('/tasks/:taskId', async (req,res) => {
+  const { taskId } = req.params; 
+  await Task.findByIdAndUpdate(taskId, req.body); 
+  res.json({
+    message: "Task updated successfully", 
+  }); 
+}); 
 
-app.delete('/tasks/:taskId', async (req, res) => {
-  const { taskId } = req.params;
-  await Task.findByIdAndRemove(taskId);
-  res.json({ message: 'Task deleted successfully' });
-});
+app.delete('/tasks/:taskId', async (req,res) => {
+  const { taskId } = req.params; 
+  await Task.findByIdAndRemove(taskId); 
+  res.json({
+    message: "Task deleted successfully", 
+  }); 
+}); 
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server listen on port ${port}`)
+})
