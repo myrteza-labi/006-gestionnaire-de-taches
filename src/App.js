2a616fe6f9208971459f7150984a75cf760f3c9e
myrteza <myrteza@hotmail.fr>
2023-09-23 18:41:57 +0200

    try 2 frontend done - by copying

--- src/App.js
+++ src/App.js
@@ -1,99 +1,93 @@
 import React, {useState, useEffect} from 'react'; 
-import axios from'axios'; 
+import axios from 'axios'; 
 
 const App = () => {
-    const [tasks, setTasks] = useState([]); 
-    const [newTask, setNewTask] = useState({
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState({
+    title: "", 
+    descritpion: "", 
+    completed: false,
+  })
+
+  const addTask = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/tasks', newTask); 
+      setTasks([...tasks, response.data]); 
+      setNewTask({
         title: "", 
-        description: "", 
+        description: "",
         completed: false,
-    })
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/tasks')
-      .then(response => {
-        setTasks(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching tasks:', error);
-      });
-  }, []);
-
-
-    const addTask = () => {
-        axios.post('http://localhost:5000/tasks', newTask)
-            .then(response => {
-                setTasks([...tasks, response.data]); 
-                setNewTask({ title: '', description : '', completed: false}); 
-            })
-            .catch(error => {
-                console.error('Error adding task:', error); 
-            }); 
+      
     }
-
-    const updateTask = (taskId, updates) => {
-        axios.put(`http://localhost:5000/tasks/${taskId}`, updates)
-        .then(() => {
-            const updatedTasks = tasks.map(task => {
-                if (task._id === taskId) {
-                    return { ...task, ...updates}; 
-                }
-                return task; 
-            })
-            setTasks(updatedTasks); 
-        })
-        .catch((error) => {
-            console.log("Error updating task", error);
-        }); 
+    catch (error) {
+      console.error('Error adding task', error)
     }
+  }
 
-    const deleteTask = (taskId) => {
-        axios.delete(`http://localhost:5000/tasks/${taskId}`)
-            .then(() => {
-                const updatedTasks = tasks.filter(task => task._id !== taskId); 
-                setTasks(updatedTasks); 
-            })
-            .catch(error => {
-                console.log("Error deleting task:", error); 
-            })
+  const updateTask = async (taskId, updates) => {
+    try {
+      await axios.put('http://localhost:5000/tasks', updates); 
+      const updatedTasks = tasks.map(task => {
+        if (task._id === taskId) {
+          return {...task, ...updates}
+        }
+        else {
+          return task;
+        }
+      }); 
+      setTasks(updatedTasks); 
     }
+    catch (error) {
+      console.error("Error updating task", error); 
+    }
+  }
 
- 
-    return (
-        <div>
-            <h1>Todo List - Backend training n° 006</h1>
-            <div>
-                <input 
-                    type="text"
-                    placeholder="Task title"
-                    value={newTask.title}
-                    onChange={(e) => setNewTask({...newTask, title: e.target.value})}
-                />
-                <input 
-                    type="text"
-                    placeholder="Task description"
-                    value={newTask.description}
-                    onChange={(e) => setNewTask({ ...newTask , description : e.target.value})}
-                />
-                <button onClick={addTask}>Add task</button>
-            </div>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task._id}>
-                        <input  
-                            type="checkbox"
-                            checked={task.completed}
-                            onChange={(e) => updateTask(task._id, {
-                                completed: e.target.checked
-                            })}
-                        />
-                        {task.title} - {task.description}
-                        <button onClick={() => deleteTask(task._id)} >Delete</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const deleteTask = async (taskId) => {
+    try {
+      await axios.delete(`http://localhost:5000/tasks/${taskId}`); 
+      const updatedTasks = tasks.filter(task => task._id !== taskId); 
+      setTasks(updatedTasks); 
+    }
+    catch (error) {
+      console.error("Error deleting task :", error)
+    }
+  }
+
+  return (
+    <>
+    <h1>Todo list - backend Training N°6</h1>
+      <input 
+        type="text"
+        placeholder="Task title"
+        onChange={e => setNewTask({...newTask, title : e.target.value})}
+      />
+      <input
+        type="text"
+        placeholder="Task description"
+        onChange={e => setNewTask({...newTask, description : e.target.value})}
+      />
+      <button onClick={addTask}>Add task</button>
+      <ul>
+        {
+          tasks && 
+          tasks.map( task => (
+            <li key={task._id}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={e => updateTask(task._id, {
+                  completed : e.target.checked
+                })}
+              />
+              {task.title} - {task.description}
+              <button onClick={() => deleteTask(task._id)} >Delete</button>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  )
 }
 
 export default App; 
--- src/server.js
+++ src/server.js
@@ -1,53 +1,46 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
-const cors = require('cors'); 
-const mongoose = require('mongoose'); 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express(); 
-app.use(cors()); 
-app.use(bodyParser.json()); 
-const port = 5000; 
+app.use(cors());
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/todolist', {
-    useNewUrlParser: true, 
-    useUnifiedTopolody: true, 
-}); 
+mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const taskSchema = new mongoose.Schema({
-    title: String,
-    description: String, 
-    completed: Boolean,
-}); 
+  title: String,
+  description: String,
+  completed: Boolean,
+});
 
-const Task = mongoose.model('Task', taskSchema); 
+const Task = mongoose.model('Task', taskSchema);
 
 app.get('/tasks', async (req, res) => {
-    const tasks = await Task.find(); 
-    res.json(tasks); 
-}); 
+  const tasks = await Task.find();
+  res.json(tasks);
+});
 
 app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body); 
-    await task.save();
-    res.json(task);  
-}); 
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
 
 app.put('/tasks/:taskId', async (req, res) => {
-    const { taskId } = req.params; 
-    await Task.findByIdAndUpdate(taskId, req.body); 
-    res.json({
-        message: "Task updated succesfully"
-    }); 
-})
+  const { taskId } = req.params;
+  await Task.findByIdAndUpdate(taskId, req.body);
+  res.json({ message: 'Task updated successfully' });
+});
 
 app.delete('/tasks/:taskId', async (req, res) => {
-    const { taskId } = req.params; 
-    await Task.findByIdAndDelete(taskId); 
-    res.json({
-        message: "Task deleted successfully"
-    }); 
-})
+  const { taskId } = req.params;
+  await Task.findByIdAndRemove(taskId);
+  res.json({ message: 'Task deleted successfully' });
+});
 
 app.listen(port, () => {
-    console.log(`Server listen to port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
