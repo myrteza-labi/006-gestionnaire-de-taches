7364316e41e11c3f321499682dd6f4c53f23806c
myrteza <myrteza@hotmail.fr>
2023-09-26 16:00:31 +0200

    backend done - autonomus 90% - try 6 done

--- src/App.js
+++ src/App.js
@@ -2,75 +2,79 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
 
 const App = () => {
+
   const [tasks, setTasks] = useState([]); 
   const [newTask, setNewTask] = useState({
     title: "", 
     description: "", 
-    completed: false, 
+    completed: false,
   })
 
   const getTasks = async () => {
     try {
       const response = await axios.get('http://localhost:5000/tasks'); 
-      setTasks(response.data)
+      const updatedTasks = response.data; 
+      setTasks(updatedTasks); 
     }
-    catch(error) {
-      console.error("Erreur lors de la recupération des tasks", error); 
+    catch (error) {
+      console.log("Erreur lors de la récupération des tasks", error)
     }
-  }
+  } 
 
   useEffect(() => {
-    getTasks()
-  }, [tasks, newTask]); 
+    getTasks(); 
+  }, [tasks])
 
   const addTask = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/tasks', newTask); 
-      console.log(response); 
+      await axios.post('http://localhost:5000/tasks', newTask); 
       setNewTask({
         title: "", 
         description: "", 
-        completed: false, 
+        completed: false,
       })
+      getTasks(); 
     }
-    catch(error) {
-      console.error("Erreur lors de l'ajout de la task", error); 
+    catch (error) {
+      console.error("Erreur lors de la création de la task", error); 
+    }
+  }
+
+  const updateTask = async (taskId, updates) => {
+    try {
+      // pourquoi passer l'id directement dans l'url ? Puis-je le passer en parametre comme les arguments ?
+      const updatedTask = await axios.put(`http://localhost:5000/tasks/${taskId}`, updates); 
+      // tester la mise à jour avec la task contenu dans la reponse de la requete
+      const updatedTasks = tasks.map((task) => {
+        if(task._id === taskId) {
+          return {...task, ...updatedTask}
+        }
+        else {
+          return task
+        }
+      })
+      setTasks(updatedTasks); 
+    }
+    catch (error) {
+      console.error("Erreur lors de la modification de la task", error); 
     }
   }
 
   const deleteTask = async (taskId) => {
     try {
-      await axios.delete(`http://localhost:5000/tasks/${taskId}`); 
-      const updatedTasks = tasks.filter((task => task._id !== taskId)); 
+      await axios.delete(`http://localhost:5000/tasks/${taskId}`)
+      const updatedTasks = tasks.filter((task) => task._id !== taskId); 
       setTasks(updatedTasks); 
     }
     catch(error) {
-      console.error("Erreur lors de la suppression de la task:", error); 
-    }; 
-  }; 
-
-  const updateTask = async (taskId, updates) => {
-    try {
-      await axios.put(`http://localhost:5000/tasks/${taskId}`, updates); 
-      const updatedTask = tasks.map((task) => {
-        if (taskId === task.id) {
-          return {...task, ...updates}; 
-        }
-        else {
-          return task; 
-        }
-      })
-      setTasks(updatedTask); 
+      console.error("Erreur lors de la suppression de la task", error); 
     }
-    catch(error) {
-      console.error("Erreur lors de la modification de la task:", error)
-    }
-  } 
+  }
 
   return (
     <div>
-      <h1>Todo list - backend training 006</h1>
-      <input
+      <h1>Todo List - backend training 006</h1>
+      <input 
         type="text"
         placeholder="Task title"
         onChange={(e) => setNewTask({...newTask, title: e.target.value})}
@@ -86,17 +90,19 @@ const App = () => {
       <ul>
         {
           tasks && 
-            tasks.map((task) => (
-              <li key={task._id}>
-                <input
-                  type="checkbox"
-                  checked={task.completed}
-                  onChange={(e) => updateTask(task._id, {...task, completed: e.target.checked})}
-                />
-                {task.title} - {task.description}
-                <button onClick={() => deleteTask(task._id)}>Delete</button>
-              </li>
-            ))
+          tasks.map((task) => (
+            <li key={task._id}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={(e) => updateTask(task._id, {
+                  completed: e.target.checked
+                })}
+              />
+              {task.title} - {task.description}
+              <button onClick={() => deleteTask(task._id)}>Delete</button>
+            </li>
+          ))
         }
       </ul>
     </div>
