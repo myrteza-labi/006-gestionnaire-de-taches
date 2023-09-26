bf1780f65c91a2951eff47b06e56c3251f46b4f1
myrteza <myrteza@hotmail.fr>
2023-09-26 21:17:18 +0200

    project done completely autonomus on first try - try 8 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import axios from 'axios'; 
 
 const App = () => {
@@ -7,44 +7,40 @@ const App = () => {
   const [newTask, setNewTask] = useState({
     title: "", 
     description: "", 
-    completed: true
+    completed: false
   })
-
-  const fetchData = async () => {
-    try {
-      const updatedTasks = await axios.get('http://localhost:5000/tasks'); 
-      setTasks(updatedTasks.data); 
-    } 
-    catch (error) {
-      console.error("Erreur lors de la récupération des tasks", error); 
-    }
-  } 
 
   useEffect(() => {
     fetchData(); 
-  },[tasks]); 
+  }, [tasks]); 
 
-  const addTask = async () => {
+  const fetchData = async () => {
     try {
-      axios.post('http://localhost:5000/tasks', newTask); 
-      setNewTask({
-        title: "", 
-        description: "", 
-        completed: false
-      })
+      const tasks = await axios.get("http://localhost:5000/tasks"); 
+      setTasks(tasks.data); 
     }
-    catch (error) {
-      console.error("Erreur lors de la création de la task", error); 
+    catch(error) {
+      console.error("Erreur lors de la récupération des tasks", error); 
     }
   }
 
-  const uploadTask = async (taskId, updates) => {
+  const addTask = async () => {
     try {
-      await axios.put(`http://localhost:5000/tasks/${taskId}`, updates); 
+      await axios.post('http://localhost:5000/tasks', newTask); 
       fetchData(); 
-    }
+    } 
     catch (error) {
-      console.error("Erreur lors de la modification de la task:", error)
+      console.error("Erreur lors de la creaction de la task", error); 
+    }
+  }
+
+  const updateTask = async (taskId, updates) => {
+    try {
+      await axios.put(`http://localhost:5000/tasks/${taskId}`, updates);
+      fetchData();  
+    } 
+    catch (error) {
+      console.error("Erreur lors de la modification de la task", error); 
     }
   }
 
@@ -53,43 +49,45 @@ const App = () => {
       await axios.delete(`http://localhost:5000/tasks/${taskId}`); 
       fetchData(); 
     }
-    catch(error) {
+    catch (error) {
       console.error("Erreur lors de la suppression de la task", error); 
-    }
-  }
+    }; 
+  }; 
 
   return (
     <div>
       <h1>Todo list - backend training n°6</h1>
-      <input 
+      <input
         type="text"
         placeholder="Task title"
         onChange={(e) => setNewTask({...newTask, title: e.target.value})}
+        value={newTask.title}
       />
       <input 
         type="text"
         placeholder="Task description"
-        onChange={(e) => setNewTask({...newTask, description: e.target.value})}
+        onChange={(e) => setNewTask({...newTask, description : e.target.value})}
+        value={newTask.description}
       />
       <button onClick={addTask}>Add task</button>
-      <ul>
-        {
-          tasks && 
-          tasks.map((task) => (
-            <li key={task._id}>
-              <input
+      {
+        tasks && 
+        <ul>
+          {
+            tasks.map((task) => (
+              <li key={task._id}>
+              <input 
                 type="checkbox"
-                onChange={(e) => uploadTask(task._id, {
-                  completed: e.target.checked
-                })}
+                onChange={(e) => updateTask(task._id, {completed: e.target.checked})}
                 checked={task.completed}
               />
               {task.title} - {task.description}
               <button onClick={() => deleteTask(task._id)}>Delete</button>
-            </li>
-          ))
-        }
-      </ul>
+              </li>
+            ))
+          }
+        </ul>
+      }
     </div>
   )
 }
